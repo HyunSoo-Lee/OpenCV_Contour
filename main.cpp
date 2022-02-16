05f2b772cf26d2ad19ffa7b4021590a4de5a8509
HyunSoo Lee <oceanyum@gmail.com>
2022-02-16 15:26:02 +0900

    function_imgRefine

--- main.cpp
+++ main.cpp
@@ -11,25 +11,21 @@ using namespace cv;
 using namespace std;
 using namespace tesseract;
 
+Mat imgRefine(Mat img, int gaussian, int canny1, int canny2);
 double  pythagoras(int  x1, int y1, int  x2, int y2);
 
 int main() {
 
 	// 1. Load image, Gray Scaling, Canny edge detection, Gaussian Blur
 	Mat img = imread("hello.jpg");
-	Mat gray;
 	resize(img, img, Size((img.size().width) / 1.5, (img.size().height) / 1.5));
-	cvtColor(img, gray, COLOR_BGR2GRAY);
-	GaussianBlur(gray, gray, Size(5, 5), 0);
-	Mat canny;
-	Canny(gray, canny, 150, 10);
-	//imshow("Canny", canny);
+	Mat result = imgRefine(img, 5, 150, 10);
 
 	// 2. Find Contours
 	vector<vector<Point>> contours;
 	//2 dimension array, number of Contours - location of The Contour
 	vector<Vec4i> hierarchy;
-	findContours(canny, contours, hierarchy, RETR_CCOMP, CHAIN_APPROX_TC89_L1);
+	findContours(result, contours, hierarchy, RETR_CCOMP, CHAIN_APPROX_TC89_L1);
 
 	// 3. Draw Contour
 	vector<vector<Point>> conpoly(contours.size());
@@ -59,8 +55,7 @@ int main() {
 	Mat trans = getPerspectiveTransform(square, warp_square);
 	warpPerspective(img, warp_img, trans, warp_size);
 	imshow("warping", warp_img);
-	cvtColor(warp_img, warp_img, COLOR_BGR2GRAY);
-	GaussianBlur(warp_img, warp_img, Size(3, 3), 0);
+	warp_img = imgRefine(warp_img, 3, 150, 10);
 	imwrite("warp_img.jpg", warp_img);
 
 	char* outText;
@@ -71,7 +66,7 @@ int main() {
 	}
 
 	// 5. Open input image with leptonica library
-	Pix* image = pixRead("warp_img.jpg");
+	Pix* image = pixRead("download.png");
 	//Pix*image = pixRead("download.png");
 	api->SetImage(image);
 
@@ -88,6 +83,15 @@ int main() {
 	waitKey();
 	return 0;
 
+}
+
+Mat imgRefine(Mat img, int gaussian, int canny1, int canny2) {
+	Mat gray;
+	Mat result;
+	cvtColor(img, gray, COLOR_BGR2GRAY);
+	GaussianBlur(gray, gray, Size(gaussian, gaussian), 0);
+	Canny(gray, result, canny1, canny2);
+	return result;
 }
 
 double  pythagoras(int  x1, int y1, int  x2, int y2) {
